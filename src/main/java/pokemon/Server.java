e4b188f44a90732694be04833c5ed4df3a033203
juanbanchero <juan.banche@gmail.com>
2018-10-04 11:28:17 -0300

    Se crearon  las cosas

--- src/main/java/pokemon/Server.java
+++ src/main/java/pokemon/Server.java
@@ -1,12 +1,14 @@
 package pokemon;
 
 import spark.Spark;
+import spark.debug.DebugScreen;
 
 public class Server {
 	public static void main(String[] args) {
 		Bootstrap.init();
 		Spark.port(9000);
 		Router.configure();
+		DebugScreen.enableDebugScreen();
 	}
 }
 
